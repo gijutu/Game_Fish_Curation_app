085aac97f40d2a2950baf42445f664bfe3ec8fba
gijutu <31819030+gijutu@users.noreply.github.com>
2018-10-11 17:10:38 +0900

    投稿機能導入

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -18,21 +18,32 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.new(game_params)
+    if @game.save
+      redirect_to games_path,notice:"大会情報を投稿しました"
+    else
+      render 'new',notice:"投稿できません"
+    end
   end
 
   def update
+    if @game.update(game_params)
+      redirect_to games_path,notice: "へんしゅうしました"
+    else
+      render 'edit'
+    end
   end
 
   def destroy
     @game.destroy
+    redirect_to games_path, notice:"投稿を削除しました"
   end
 
   private
-  def set_blog
+  def set_game
     @game = Game.find(params[:id])
   end
 
   def game_params
-    params.require(:game).permit(:title, :content)
+    params.require(:game).permit(:title, :content, :created_at, :updated_at, :the_day)
   end
 end
