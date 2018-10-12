d74e729e56651e1bcc0357b3bdea25605cbce1e8
gijutu <31819030+gijutu@users.noreply.github.com>
2018-10-12 11:42:26 +0900

    TOPコントローラー作成、bootstrap4導入

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'games#index'
   resources :games
+  get 'tops/index'
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
