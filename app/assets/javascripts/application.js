d4d874cf22ff81eca8c8d7b3bb40d0c12fa52f9d
gijutu <31819030+gijutu@users.noreply.github.com>
2018-10-30 15:30:42 +0900

    assets js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery/dist/jquery.js
 //= require popper.js/dist/popper.js
 //= require bootstrap/dist/js/bootstrap.js
-//= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
