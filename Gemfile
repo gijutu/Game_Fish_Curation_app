eaa6177322969416a52844585751af88f04d1158
gijutu <31819030+gijutu@users.noreply.github.com>
2018-10-30 16:50:57 +0900

    assets js pack

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'sass-rails', '~> 5.0'
 gem 'turbolinks', '~> 5'
 gem 'uglifier', '>= 1.3.0'
 gem 'webpacker', '~> 3.5'
+gem 'bootstrap', '~> 4.1.3'
+gem 'jquery-rails'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery/dist/jquery.js
-//= require popper.js/dist/popper.js
-//= require bootstrap/dist/js/bootstrap.js
+//= require jquery
+//= require jquery_ujs
+//= require jquery3
+//= require popper
+//= require bootstrap
 //= require activestorage
-//= require turbolinks
 //= require_tree .
